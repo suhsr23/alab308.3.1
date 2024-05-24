babe437021135ed6ad680b22f75e8345712b6f7b
suhsr23 <suhsr23@gmail.com>
2024-05-24 15:29:44 -0400

    first commit

---
+++ part2.js
@@ -0,0 +1,17 @@
+let num= 59;
+
+for (let i=num+1;i<=100;i++)
+{
+    let isPrime = true;
+    for(j=num; j > 1; j--)
+    {
+        if (i % j === 0)
+
+        isPrime = false;
+    }
+    if(isPrime)
+    {
+        console.log(i);
+        break;
+    } 
+}
